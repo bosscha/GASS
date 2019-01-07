943bd18608f34c0c21bc82f18912ea13c2066ef5
stephane <steph.leon@gmail.com>
2019-01-07 10:16:34 -0300

    install julia source

---
+++ src/install.jl
@@ -0,0 +1,16 @@
+## install package for Gaia.Clustering
+
+using Pkg
+
+Pkg.add("PyCall")
+Pkg.add("DataFrames")
+Pkg.add("Clustering")
+Pkg.add("Statistics")
+Pkg.add("Distributions")
+Pkg.add("Random")
+Pkg.add("CSV")
+Pkg.add("PyPlot")
+Pkg.add("Formatting")
+Pkg.add("Query")
+
+println("## Package installation for GaiaClustering done...")
